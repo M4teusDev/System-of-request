9980cdad63e46b5a06443ca7a7eec1ce01afe547
Mateus Prado <mateus.silvaprado@gmail.com>
2020-09-30 01:44:17 -0300

    SystemOfRequest_2.1

--- ac1/src/main/java/com/example/ac1/Repository/RequestRepository.java
+++ ac1/src/main/java/com/example/ac1/Repository/RequestRepository.java
@@ -1,24 +1,32 @@
 package com.example.ac1.Repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 
 import com.example.ac1.model.Request;
 
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class RequestRepository {
     
-    private List<Request> listOfRequests;
+
+    private List<Request> listOfRequests = new ArrayList<Request>();
     private int nextCode = 0;
 
 
-    @PostConstruct
-    public void saveRequest (Request request) {
+    //@PostConstruct
+    public Request saveRequest (Request request) {
 
         request.setCod(++nextCode);
         request.setDate(LocalDateTime.now());
         listOfRequests.add(request);
+
+        return request;
     }
 
     public List<Request> getAllRequests() {
--- ac1/src/main/java/com/example/ac1/controller/RequestController.java
+++ ac1/src/main/java/com/example/ac1/controller/RequestController.java
@@ -1,15 +1,65 @@
 package com.example.ac1.controller;
 
+import java.net.URI;
+import java.util.List;
 
+import com.example.ac1.Repository.RequestRepository;
+import com.example.ac1.model.Request;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/Request")
+@RequestMapping("/request")
 
 public class RequestController {
     
-    // @Autowired
-    // private RepositoryRequest request;
+    @Autowired
+    private RequestRepository requestRepository;
+
+    @PostMapping()
+    public ResponseEntity<Void> save (@RequestBody Request request) {
+        request = requestRepository.saveRequest(request);
+        URI uri = URI.create("http://localhost:8080/request/" + request.getCod());
+        return ResponseEntity.created(uri).build();
+    }
+    
+    @GetMapping()
+    public List<Request> getAllRequests() {
+        return requestRepository.getAllRequests();
+    }
+    
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Request> getRequestsByCode(@PathVariable int codigo) {
+
+        Request request = requestRepository.getRequestByCode(codigo);
+
+        if( request != null){
+            return ResponseEntity.ok(request);   
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }            
+    }
+
+    @DeleteMapping("/{codigo}")
+    public ResponseEntity<Void> remover(@PathVariable int codigo){   
+        Request request = requestRepository.getRequestByCode(codigo);
+        
+        if(request != null){
+            requestRepository.removeRequest(request);
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
