1a8a86486eb35090999170714e38c8bcc72771ad
Mateus Prado <mateus.silvaprado@gmail.com>
2020-09-29 22:10:51 -0300

    SystemOfRequest_v3

---
+++ ac1/src/main/java/com/example/ac1/controller/RequestController.java
@@ -0,0 +1,15 @@
+package com.example.ac1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/Request")
+
+public class RequestController {
+    
+    // @Autowired
+    // private RepositoryRequest request;
+}
---
+++ ac1/src/main/java/com/example/ac1/model/Request.java
@@ -0,0 +1,5 @@
+package com.example.ac1.model;
+
+public class Request {
+    
+}
